5aa28515b99de33e6be9e16c738824c22cf6105b
staspitsyk <staspitsyk@gmail.com>
2020-07-04 14:42:12 +0300

    added relationship between tables

--- src/modules/groups/groups.model.js
+++ src/modules/groups/groups.model.js
@@ -1,6 +1,11 @@
 const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../../db');
 
+const GroupItems = require('./groups-item.model');
+const Teachers = require('../teachers/teachers.model');
+const Students = require('../students/students.model');
+const Lessons = require('../lessons/lessons.model');
+
 class GroupsModel extends Model {}
 
 const Groups = GroupsModel.init(
@@ -19,4 +24,21 @@ const Groups = GroupsModel.init(
   { sequelize }
 );
 
+Students.belongsTo( Groups, { foreignKeyConstraint: true, foreignKey: 'groupId', targetKey: 'id' } );
+
+Groups.hasMany( Students, { foreignKeyConstraint: true, foreignKey: 'groupId', targetKey: 'id' } );
+
+Lessons.belongsTo( Groups, { foreignKeyConstraint: true, foreignKey: 'groupId', targetKey: 'id' } );
+
+Lessons.belongsTo( Teachers, { foreignKeyConstraint: true, foreignKey: 'teacherId', targetKey: 'id' } );
+
+GroupItems.belongsTo( Teachers, { foreignKeyConstraint: true, foreignKey: 'teacherId', targetKey: 'id' } );
+
+GroupItems.belongsTo( Groups, { foreignKeyConstraint: true, foreignKey: 'groupId', targetKey: 'id' } );
+
+Groups.hasMany( GroupItems, { foreignKeyConstraint: true, foreignKey: 'groupId', targetKey: 'id' } );
+
+Teachers.hasMany( GroupItems, { foreignKeyConstraint: true, foreignKey: 'teacherId', targetKey: 'id' } );
+
 module.exports = Groups;
+
