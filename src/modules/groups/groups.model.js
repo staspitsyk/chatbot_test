56b7c287fe19e8ed5bd73009f2f7e410dc31476e
staspitsyk <staspitsyk@gmail.com>
2020-07-04 12:46:55 +0300

    Added lessons model

--- src/modules/groups/groups.model.js
+++ src/modules/groups/groups.model.js
@@ -11,11 +11,10 @@ const Groups = GroupsModel.init(
       allowNull: false,
       autoIncrement: true,
     },
-    firstName: { type: DataTypes.STRING(), allowNull: false },
-    lastName: { type: DataTypes.STRING(), allowNull: false },
-    email: { type: DataTypes.STRING(), allowNull: true },
-    phoneNumber: { type: DataTypes.STRING(), allowNull: true },
-    password: { type: DataTypes.STRING(), allowNull: false },
+    groupName: {
+        type: DataTypes.STRING(),
+        allowNull: false,
+      },
   },
   { sequelize }
 );
